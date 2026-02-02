eb4f0932dfc0b9caf9f6aa5608bd20c296d9f256
DEV KUMAR SHARMA <166902599+0001expect@users.noreply.github.com>
2026-02-02 11:03:13 +0530

    Delete Backend-Dev-nodejs-programming/Backend-Dev-nodejs-programming directory

--- Backend-Dev-nodejs-programming/Backend-Dev-nodejs-programming/Assignment/System-monitor/app.js
+++
@@ -1,8 +0,0 @@
-const getSystemInfo = require('./systeminfo');
-const logData = require('./logger');
-
-setInterval(() => {
-  const systemInfo = getSystemInfo();
-  logData(systemInfo);
-  console.log('System info logged...');
-}, 5000);
